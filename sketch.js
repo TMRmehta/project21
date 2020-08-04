014e21f5df062e971b1630be7fffe089fa944cc9
TMRmehta <68707216+TMRmehta@users.noreply.github.com>
2020-08-03 21:27:39 -0400

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,39 @@
+var wall, thickness;
+var bullet,speed,weight;
+var damage;
+
+function setup() {
+  createCanvas(1600,400);
+ wall=createSprite(1200,200,thickness, height/2);
+ wall.shapeColor=color(80,80,80);
+ thickness=random(22,83);
+ speed=random(223.321);
+ weight=random(30,52);
+}
+
+function draw() {
+  background(255,255,255);
+  //hasCollided();
+  if(hasCollided(bullet,wall)){
+    bullet.velocityX=0;
+     damage=0.5*weight*speed*speed/(thickness*thickness*thickness);
+
+   
+  if(damage>10){
+    wall.shapeColor=color(255,0,0);
+  } 
+
+  if (damage<10){
+    wall.shapeColor=color(0,255,0);
+  }
+}
+  drawSprites();
+}
+function hasCollided (lbullet, lwall) {
+  bulletRightEdge=lbullet.x +lbullet.width;
+  wallleftEdge=lwall.x;
+  if (bulletRightEdge>=wallleftEdge){
+    return true;
+  }
+  return false;
+}
